842887f30774aba04bfabf41d8c5456ab9e3b332
unknown <usinaleksandr801@gmail.com>
2023-07-04 15:30:55 +0600

    add formatting

--- Scripts/Requests.sql
+++ Scripts/Requests.sql
@@ -1,25 +1,40 @@
 /*Запрос на количество активных и неактивных пользователей + их процентное соотношение (подзапрос)*/
-select is_active,
+select
+	is_active,
 	count(*) as Количество,
-	round(count(id)*100.0 / (select count(*) from users), 2) as Соотношение
-from users
-group by is_active
+	round(count(id)* 100.0 / (select count(*) from users), 2) as Соотношение
+from
+	users
+group by
+	is_active
 
 /*Запрос на количество пользователей, которые зарегестрировались от определенной компании*/
-select c.name as Компания, count(*) as Количество_пользователей  
-from users u
+select
+	c.name as Компания,
+	count(*) as Количество_пользователей
+from
+	users u
 join company c
-on u.company_id = c.id
-group by c.name
-order by Количество_пользователей desc
+on
+	u.company_id = c.id
+group by
+	c.name
+order by
+	Количество_пользователей desc
 
 /*Запрос на количество пользователей, которые зарегестрировались от определенной компании + пользователи без компаний*/
-select coalesce(c.name, 'Без компании') as Компания, count(*) as Количество_пользователей  
-from users u
+select
+	coalesce(c.name, 'Без компании') as Компания,
+	count(*) as Количество_пользователей
+from
+	users u
 left join company c
-on u.company_id = c.id
-group by c.name
-order by Количество_пользователей desc
+on
+	u.company_id = c.id
+group by
+	c.name
+order by
+	Количество_пользователей desc
 
 /*
 select *, to_char(entry_at, 'YYYY-MM-DD') as Дата_входа 
@@ -29,18 +44,36 @@ group by user_id
 
 
 /*Запрос на количество авторизаций для каждого пользователя*/
-select user_id , count(*) as Количество_авторизаций 
-from userentry u
-group by user_id
-order by Количество_авторизаций desc
+select
+	user_id ,
+	count(*) as Количество_авторизаций
+from
+	userentry u
+group by
+	user_id
+order by
+	Количество_авторизаций desc
 
 /*CTE - Common Table Expressions - результаты запроса, которые можно использовать множество раз в других запросах*/
 /*Обобщенные табличные выражения - временные таблицы, которые будут жить только во время выполнения запроса, можно использовать в качестве виртуального источника данных*/
 with temp_cte as (
-select user_id , count(*) as Количество_авторизаций 
-from userentry u
-group by user_id
+select
+	user_id ,
+	count(*) as Количество_авторизаций
+from
+	userentry u
+group by
+	user_id
 )
-select *
-from temp_cte
-order by Количество_авторизаций desc
+select
+	*,
+	case
+		when temp_cte.Количество_авторизаций > 50 then 'A'
+		when temp_cte.Количество_авторизаций > 20 then 'B'
+		when temp_cte.Количество_авторизаций > 1 then 'C'
+		else 'D'
+	end Группы_пользователей
+from
+	temp_cte
+order by
+	Количество_авторизаций desc
